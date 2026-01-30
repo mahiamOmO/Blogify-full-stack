65d9e9dde3a61a95c415b11b9b7127aeca26ec8d
mahiamOmO <129661915+mahiamOmO@users.noreply.github.com>
2026-01-30 22:17:18 +0600

    server created

---
+++ server.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const express = require('express');
+
+
+//!server
+
+const app = express();
+
+const server = http.createServer(app);
+//? Start the server
+
+const PORT = process.env.PORT ||3000
+server.listen(PORT,console.log(`Server is running on port ${PORT}`));
