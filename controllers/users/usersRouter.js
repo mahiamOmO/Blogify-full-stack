b3f6ad6d938a0a5c65c9c1bc1b5fb8b78074ec11
mahiamOmO <129661915+mahiamOmO@users.noreply.github.com>
2026-07-05 16:21:27 +0600

    refactor: fix folder structure and routing paths

--- controllers/users/usersRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const userRouter = express.Router();
-
-// Register
-userRouter.post('/api/v1/users/register', register);
-
-// Export
-module.exports = userRouter;
--- routes/users/userRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { register } = require('../../controllers/users/usersCtrls');
-
-const userRouter = express.Router();
-
-// Register
-userRouter.post('/api/v1/users/register', register);
-
-module.exports = userRouter;
